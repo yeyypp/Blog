96bbf98a9ca355e01fdb7a0348e19631840f4357
Ye <18610188361@163.com>
2019-04-11 17:41:06 +0800

    change

--- 学习总结/Spring/Bean.java
+++ 学习总结/Spring/Bean.java
@@ -1,5 +1,16 @@
 public class Main {
     /**
-     *
+     * Bean配置的三种方式
+     * https://blog.csdn.net/icarus_wang/article/details/51649635
+     */
+
+    /**
+     * Bean生命周期
+     * https://blog.csdn.net/ysjian_pingcx/article/details/8968591
+     * http://wiki.jikexueyuan.com/project/spring/dependency-injection.html
+     */
+
+    /**
+     * applicationcontext 初始化
      */
 }
---
+++ 学习总结/Spring/SprintMVC.java
@@ -0,0 +1,6 @@
+public class Main {
+    /**
+     * https://www.cnblogs.com/winterfells/p/8476759.html
+     * https://blog.csdn.net/eson_15/column/info/spring-mvc
+     */
+}
